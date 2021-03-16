ecc074bdc7470d80d2e04de8417b4016a4adec73
nick <nhjlopez@gmail.com>
2021-03-16 16:33:31 -0700

    Did it for one type of photo

--- src/index.ts
+++ src/index.ts
@@ -2,146 +2,206 @@ import { promises as fs } from 'fs';
 import * as path from 'path';
 
 // convert to class and return errors
-const imageLocation = path.join(
-  path.dirname(__dirname),
-  // '/images/sample_1280x853.bmp'
-  '/images/testPic001.bmp'
-);
-const getImageBuffer = async () => {
-  const data = await fs.readFile(imageLocation);
-  const buffer = Buffer.from(data);
-  console.log(buffer);
-  return buffer;
-};
 
-const copyBufferHeader = (ogBuffer: Buffer, newBuffer: Buffer) => {
-  for (let i = 0; i < 54; i++) {
-    newBuffer[i] = ogBuffer[i];
+type mutatorFunction = () => void; // https://www.typescriptlang.org/docs/handbook/2/functions.html
+class BMPBuffer {
+  imagePath: string | null;
+  ogImageBuffer: Buffer;
+  negativeBuffer: null | Buffer;
+  bitsPerPixel: number;
+  fileName: string | null;
+
+  constructor(bufferSamp: Buffer) {
+    this.imagePath = null;
+    this.fileName = null;
+    this.ogImageBuffer = bufferSamp;
+    this.negativeBuffer = null;
+    this.bitsPerPixel = 24;
   }
-};
 
-const readBufferHeader = (buffer: Buffer) => {
-  let pos = 2;
-  let fileSize = buffer.readUInt32LE(pos); // (54 bytes header + 16 bytes data)
-  pos += 4;
-  let reserved = buffer.readUInt32LE(pos);
-  pos += 4;
-  let offset = buffer.readUInt32LE(pos);
-  pos += 4;
-  let headerSize = buffer.readUInt32LE(pos); // number of bytes in the db header from this point
-  pos += 4;
-  let width = buffer.readUInt32LE(pos);
-  pos += 4;
-  let height = buffer.readInt32LE(pos);
-  pos += 4;
-  let planes = buffer.readUInt16LE(pos);
-  pos += 2;
-  let bitsPerPixel = buffer.readUInt16LE(pos); // 24 bits
-  pos += 2;
-  let compress = buffer.readUInt32LE(pos);
-  pos += 4;
-  let rawSize = buffer.readUInt32LE(pos); // Size of the raw bitmap data (including padding) filesize - offset
-  pos += 4;
-  let hr = buffer.readUInt32LE(pos); // Horizontal Rez
-  pos += 4;
-  let vr = buffer.readUInt32LE(pos); // Vertical Rez
-  pos += 4;
-  let colors = buffer.readUInt32LE(pos);
-  pos += 4;
-  let importantColors = buffer.readUInt32LE(pos); //32h  zero important colors means all colors are important
-  // pos += 4; // Start of pixel array (bitmap data)
-  // let redPixel = buffer.readUInt2
+  setImagePath(pathToPic: string) {
+    try {
+      this.imagePath = path.join(
+        path.dirname(__dirname),
+        `${pathToPic}`
+      );
+      this.fileName = path.basename(this.imagePath);
+    } catch (err) {
+      console.error(String(err));
+      let errMsg = new Error(
+        'Please Provide A Correct Path To The File'
+      );
+      return { err, errMsg };
+    }
+  }
 
-  // console.log(pos, buffer.readUInt8(pos)); // 33
-  // console.log(pos, buffer.readUInt8(pos++)); // 34
-  // console.log(pos, buffer.readUInt8(pos++)); // 35
-  // console.log(pos, buffer.readUInt8(pos++)); // 36
-  // console.log(pos, buffer.readUInt8(pos++)); // 37
-  // console.log(pos, buffer.readUInt8(pos++)); // 38
-  // console.log(pos, buffer.readUInt8(pos++)); // 39
-  // console.log(pos, buffer.readUInt8(pos++)); // 40
-  return {
-    fileSize,
-    reserved,
-    offset,
-    headerSize,
-    width,
-    height,
-    planes,
-    bitsPerPixel,
-    compress,
-    rawSize,
-    hr,
-    vr,
-    colors,
-    importantColors,
+  async getBMPBuffer(imagePath: string) {
+    console.log('Reading File');
+    try {
+      const data = await fs.readFile(imagePath);
+      const buffer = Buffer.from(data);
+      return buffer;
+    } catch (err) {
+      throw new Error('The URL Is Incorrect or File Reading Failed');
+      return null;
+    }
+  }
+
+  copyBMPHeader(ogBuffer: Buffer, negativeBuffer: Buffer) {
+    console.log('Copying ogBMP Header');
+    for (let i = 0; i < 54; i++) {
+      negativeBuffer[i] = ogBuffer[i];
+    }
+    console.log('Copying Complete', negativeBuffer);
+  }
+
+  async createNegativeBMP(): Promise<any> {
+    this.negativeBuffer = Buffer.alloc(this.ogImageBuffer!.length);
+
+    this.copyBMPHeader(this.ogImageBuffer!, this.negativeBuffer);
+
+    // traverse and inverse colors
+    let start = 54;
+    for (let i = start; i < this.negativeBuffer.length; i += 3) {
+      let r = this.ogImageBuffer.readUInt8(i);
+      let g = this.ogImageBuffer.readUInt8(i + 1);
+      let b = this.ogImageBuffer.readUInt8(i + 2);
+      const { newR, newG, newB } = this.invertRGB(r, g, b); // need to convert to base 64 i believe
+
+      this.negativeBuffer.writeUInt8(newR, i);
+      this.negativeBuffer.writeUInt8(newG, i + 1);
+      this.negativeBuffer.writeUInt8(newB, i + 2);
+    }
+
+    // write bmp file utilizing negative buffer
+    let img = await this.writeImage(this.negativeBuffer);
+
+    return img;
+    // cleanup?
+  }
+
+  writeImage = (buffer: Buffer, fileName: string = 'sick') => {
+    return fs.writeFile(`${fileName}-negative.bmp`, buffer);
   };
-};
 
-const manipulateBuffer = (buffer: Buffer) => {
-  const start = 54;
-  const finish = 1077;
-  var num = 1;
-  for (let i = start; i <= finish; i++) {
-    if (num % 4 !== 0) buffer[i] = 255 - buffer[i];
-    num++;
+  invertRGB(r: number, g: number, b: number) {
+    let newR: number = 255 - r;
+    let newG: number = 255 - g;
+    let newB: number = 255 - b;
+
+    return { newR, newG, newB };
   }
-};
 
-const traverseBuffer = (buffer: Buffer) => {
-  let start = 54;
-  for (let i = start; i < buffer.length; i += 3) {
-    let r = buffer.readUInt8(i);
-    let g = buffer.readUInt8(i + 1);
-    let b = buffer.readUInt8(i + 2);
-    const { newR, newG, newB } = invertRGB(r, g, b);
-    buffer[i] = newR;
-    buffer[i + 1] = newG;
-    buffer[i + 2] = newB;
+  imageIsBMP(buffer: Buffer) {
+    console.log(
+      'Buffer Image is BMP FORMAT',
+      buffer.toString('utf-8', 0, 2) == 'BM'
+    );
+    return buffer.toString('utf-8', 0, 2) == 'BM';
   }
-};
 
-const invertRGB = (r: number, g: number, b: number) => {
-  let newR: number = 255 - r;
-  let newG: number = 255 - g;
-  let newB: number = 255 - b;
+  analyzeBMP(buffer: Buffer) {
+    console.log('Analyzing...');
+    if (!this.imageIsBMP(buffer)) {
+      throw Error('File is not a BMP format');
+      return null;
+    }
 
-  return { newR, newG, newB };
-};
+    let headerData = this.extractBMPHeader(buffer);
+    try {
+      console.log('Confirming BMP Image is 24 BitsPerPixel');
+      if (headerData.bitsPerPixel !== 24) {
+        throw new Error('The Image must be in 24 BPP format');
+      }
+      console.log('Confirmed');
+    } catch (err) {
+      console.log('Uh Oh...', err);
+      return null;
+    }
+  }
 
-const createImage = async (buffer: Buffer) => {
-  await fs.writeFile('negativeImage.bmp', buffer);
-};
+  extractBMPHeader(buffer: Buffer) {
+    let pos = 2;
+    let fileSize = buffer.readUInt32LE(pos); // (54 bytes header + 16 bytes data)
+    pos += 4;
+    let reserved = buffer.readUInt32LE(pos);
+    pos += 4;
+    let offset = buffer.readUInt32LE(pos);
+    pos += 4;
+    let headerSize = buffer.readUInt32LE(pos); // number of bytes in the db header from this point
+    pos += 4;
+    let width = buffer.readUInt32LE(pos);
+    pos += 4;
+    let height = buffer.readInt32LE(pos);
+    pos += 4;
+    let planes = buffer.readUInt16LE(pos);
+    pos += 2;
+    let bitsPerPixel = buffer.readUInt16LE(pos); // 24 bits
+    pos += 2;
+    let compress = buffer.readUInt32LE(pos);
+    pos += 4;
+    let rawSize = buffer.readUInt32LE(pos); // Size of the raw bitmap data (including padding) filesize - offset
+    pos += 4;
+    let hr = buffer.readUInt32LE(pos); // Horizontal Rez
+    pos += 4;
+    let vr = buffer.readUInt32LE(pos); // Vertical Rez
+    pos += 4;
+    let colors = buffer.readUInt32LE(pos);
+    pos += 4;
+    let importantColors = buffer.readUInt32LE(pos); //32h  zero important colors means all colors are important
+    // pos += 4; // Start of pixel array (bitmap data)
+    // let redPixel = buffer.readUInt2
 
-const convertImage = async () => {
-  let originalImageBuffer = await getImageBuffer();
+    // Check to make sure the Bits Per Pixel is 24
 
-  if (originalImageBuffer.toString('utf-8', 0, 2) != 'BM') {
-    throw new Error('The File Format Must Be in a .BMP');
+    return {
+      fileSize,
+      reserved,
+      offset,
+      headerSize,
+      width,
+      height,
+      planes,
+      bitsPerPixel,
+      compress,
+      rawSize,
+      hr,
+      vr,
+      colors,
+      importantColors,
+    };
+  }
+}
+
+const getBMPBuffer = async (imagePath: string) => {
+  console.log('Reading File');
+  try {
+    const data = await fs.readFile(imagePath);
+    const buffer = Buffer.from(data);
+    return buffer;
+  } catch (err) {
+    throw new Error('The URL Is Incorrect or File Reading Failed');
     return null;
   }
-
-  //  const bufferHeader = readBufferHeader(originalImageBuffer);
-
-  let bufferCopy: Buffer = Buffer.alloc(originalImageBuffer.length);
-
-  copyBufferHeader(originalImageBuffer, bufferCopy);
-
-  console.log(
-    bufferCopy,
-    originalImageBuffer[originalImageBuffer.length - 1],
-    bufferCopy[bufferCopy.length - 1]
-  );
-  // manipulateBuffer(bufferCopy);
-  // createImage(bufferCopy);
 };
 
-convertImage();
+// '/images/sample_1280x853.bmp'
+// '/images/testPic001.bmp'
 
-const toBase64 = (arr: any[]) => {
-  //arr = new Uint8Array(arr) if it's an ArrayBuffer
-  return arr.reduce(
-    (data, byte) => data + String.fromCharCode(byte),
-    ''
+const readAndCreateNegative = async () => {
+  let imagePath = path.join(
+    path.dirname(__dirname),
+    `${'/images/sample_1280x853.bmp'}`
   );
+
+  let buffer = await getBMPBuffer(imagePath);
+  let bmp;
+
+  if (buffer) {
+    bmp = new BMPBuffer(buffer);
+  }
+
+  let negative = await bmp?.createNegativeBMP();
+  console.log(negative);
 };
+readAndCreateNegative();
